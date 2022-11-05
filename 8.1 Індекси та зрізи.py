2f32ff6e149154262691177291df2b5aaad9c8b9
Ярослав Каніковський <110105516+KanikovckiyYaroslav@users.noreply.github.com>
2022-11-05 12:35:17 +0200

    Create 8.1 Індекси та зрізи.py

---
+++ 8.1 Індекси та зрізи.py
@@ -0,0 +1,27 @@
+"Індекси та зрізи"
+# Рядки
+a = "Hello"
+
+# Індекс
+print(a[0])
+print(a[1])
+print(a[2])
+
+# Зворотній індекс
+print(a[-1])
+print(a[-2])
+print(a[-3])
+
+# Зріз
+print(a[:])
+print(a[1:4])
+print(a[-5:-1])
+
+# Шаг
+print(a[::])
+print(a[::2])
+print(a[::-2])
+
+# Зріз з шагом
+print(a[1:5:2])
+print(a[-1:-5:-2])
